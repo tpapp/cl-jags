0f0f98751d3e2d9f6f02ecbdb74fe7ade804cd98
tpapp <tkpapp@gmail.com>
2011-02-18 11:12:54 +0100

    Rewrote DESTRUCTURED-VARIABLES, now this should be correct.

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -2,12 +2,36 @@
 
 (in-package #:cl-jags)
 
-(defun destructured-variables (lambda-list)
-    "List all variables in lambda-list."
-    ;; !! includes INIT-FORMs if they are symbols.  This is good enough for
-    ;; !! creating an IGNORABLE declaration, but not for other applications.
-    (remove-if (lambda (var)
-                 (typep var '(or null
-                              (not symbol)
-                              (member &body &rest &key &optional))))
-               (flatten lambda-list)))
+(defun destructured-variables (macro-lambda-list)
+    "List all bound variables in a macro lambda list, assuming that the latter
+is syntactically correct."
+  (let (variables
+        key-or-optional?
+        (special-keywords '((&optional t)
+                            (&rest nil)
+                            (&key t)
+                            (&allow-other-keys)
+                            (&aux)
+                            (&whole)
+                            (&body)
+                            (&environment))))
+    (flet ((process (var)
+             (acond
+               ((find var special-keywords :key #'first)
+                (awhen (second it)      ; switch mode when applicable
+                  (setf key-or-optional? it)))
+               ((listp var)
+                (if key-or-optional?
+                    (progn
+                      (push (first var) variables)
+                      (awhen (third var) ; supplied-p
+                        (push it variables)))
+                    (appendf variables (destructured-variables var))))
+               (t (push var variables)))))
+      (let ((last (last macro-lambda-list)))
+        (aif (cdr last)                 ; dotted list
+             (progn
+               (push it variables)
+               (mapc #'process (butlast macro-lambda-list)))
+             (mapc #'process macro-lambda-list)))
+      variables)))
